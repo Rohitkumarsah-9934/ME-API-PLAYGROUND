3182ec8fb793c60da50e9e806f091aadaa115737
Rohit Kumar Sah <rohitkumar912804@gmail.com>
2026-02-05 19:08:31 +0530

    Configure CORS middleware with credentials and origin

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,10 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+   credentials: true,
+    origin :  "http://localhost:3000"
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
